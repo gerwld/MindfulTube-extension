98502f4d4fd6222d82ff732cddfa3c4e7bb46940
P Jaworski <pjaworski.dev@gmail.com>
2023-07-09 23:13:32 +0200

    Init obf. code

---
+++ assets/js/store.js
@@ -0,0 +1,16 @@
+//   - This file is part of MindfulTube Extension
+//  <https://github.com/gerwld/MindfulTube-extension/blob/main/README.md>,
+//   - Copyright (C) 2019-present MindfulTube Extension, Inc.
+//   -
+//   -MindfulTube Extension is free software: you can redistribute it and/or modify
+//   - it under the terms of the Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) License.
+//   -
+//   -MindfulTube Extension is distributed in the hope that it will be useful,
+//   - but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   - Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) License for more details.
+//   -
+//   - You should have received a copy of the Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) License
+//   - along withMindfulTube Extension.  If not, see <https://creativecommons.org/licenses/by-nc/4.0/>.
+
+const _0x3fef64=_0x2309;(function(_0x54b8df,_0x594a5e){const _0x1ed4f7=_0x2309,_0x1363ca=_0x54b8df();while(!![]){try{const _0x148dd5=parseInt(_0x1ed4f7(0x80))/0x1*(-parseInt(_0x1ed4f7(0x82))/0x2)+parseInt(_0x1ed4f7(0x85))/0x3*(parseInt(_0x1ed4f7(0x86))/0x4)+parseInt(_0x1ed4f7(0x89))/0x5+-parseInt(_0x1ed4f7(0x83))/0x6+-parseInt(_0x1ed4f7(0x84))/0x7+-parseInt(_0x1ed4f7(0x81))/0x8+parseInt(_0x1ed4f7(0x87))/0x9;if(_0x148dd5===_0x594a5e)break;else _0x1363ca['push'](_0x1363ca['shift']());}catch(_0x20d930){_0x1363ca['push'](_0x1363ca['shift']());}}}(_0x53be,0x79bf4));export const initialState={'_0x38b72b':!![],'_0x2474b4':!![],'_0x1a2d43':!![],'_0x5853ce':!![],'_0x3e46cf':!![],'_0x2d1817':![],'_0x26a752':![],'_0xc2f1b2':!![],'_0x546778':_0x3fef64(0x8a),'font':'default'};function _0x2309(_0xd74fae,_0xf9b51){const _0x53bec3=_0x53be();return _0x2309=function(_0x2309f7,_0x48cea6){_0x2309f7=_0x2309f7-0x7f;let _0x1135e4=_0x53bec3[_0x2309f7];return _0x1135e4;},_0x2309(_0xd74fae,_0xf9b51);}function _0x53be(){const _0x3b47da=['185712iDvKrL','1596378uDUHDi','133251zjQROz','52XEuWEN','9791811rlzxoo','get','1269590WjvjJi','default','storage','formState','1BLcllJ','2507600lhCskR','1696372bHImxo'];_0x53be=function(){return _0x3b47da;};return _0x53be();}export function initStateIfNotExist(){const _0x1d4aa0=_0x3fef64;chrome[_0x1d4aa0(0x8b)]['_0x15ae9d'][_0x1d4aa0(0x88)](_0x1d4aa0(0x7f),_0xcef7df=>{const _0x36d4de=_0x1d4aa0;if(!_0xcef7df[_0x36d4de(0x7f)])chrome[_0x36d4de(0x8b)]['_0x15ae9d']['set']({'formState':{...initialState}});});}
